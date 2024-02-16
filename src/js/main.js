63d12749de2e8f6aca810fa66025b5e83a287cc3
Anton <erikssonanton94@gmail.com>
2024-02-16 10:41:21 +0100

    Lagt till klickfunktion på en av animeringarna

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,18 @@ function toggleMenu() {
     }
 
 }
+
+let square2El = document.getElementById('square2');
+square2El.addEventListener('click', flip)
+
+function fli() {
+    document.getElementById('square2').className += ' flipme';
+}
+
+function flip() {
+    document.getElementById('square2').className += ' flipme';
+    setTimeout(function () {
+        document.getElementById('square2').className += 'square2'
+    }, 2000);
+}
+
